257e98489a684cba2667a827e79e23d12bf3cd4c
stillrdtr <stillrdtr@gmail.com>
2026-03-30 02:01:59 -0700

    A

--- buttons/closeTicketButton.js
+++ buttons/closeTicketButton.js
@@ -2,39 +2,72 @@ module.exports = {
   customId: "p_285689439538122754",
 
   async execute(interaction) {
-
-    const STAFF_ROLE_IDS = ["1488071288970023035", "1488033004562677930"]
+    const REQUIRED_ROLE_ID = "PUT_STAFF_ROLE_ID_HERE";
 
     const isAdmin = interaction.member.permissions.has("Administrator");
-    const hasRole = interaction.member.roles.cache.has(STAFF_ROLE_IDS);
+    const hasRole = interaction.member.roles.cache.has(REQUIRED_ROLE_ID);
 
     if (!isAdmin && !hasRole) {
       return interaction.reply({
         content: "<:rose_xMark:1488048189255716945> You do **not** have **permission** to use this button.",
-        ephemeral: true
+        flags: 64
       });
     }
 
-    // must be a ticket channel (topic = user id)
     if (!interaction.channel.topic || !/^\d+$/.test(interaction.channel.topic)) {
       return interaction.reply({
         content: "<:rose_xMark:1488048189255716945> This is not a valid **ticket** channel.",
-        ephemeral: true
+        flags: 64
       });
     }
 
     try {
+      const user = await interaction.client.users.fetch(interaction.channel.topic).catch(() => null);
 
-      await interaction.reply({
+      if (user) {
+        await user.send({
   "flags": 32768,
-  ephemeral: true,
   "components": [
     {
       "type": 17,
       "components": [
         {
           "type": 10,
-          "content": "<a:sea_loading:1466357864477098014> Closing ticket..."
+          "content": "# Ticket Closed"
+        },
+        {
+          "type": 10,
+          "content": "Your ticket in **.rose** has been closed. If you need further assistance, do not hesitate to contact us again. We hope you enjoyed your experience with our team."
+        },
+        {
+          "type": 14,
+          "spacing": 2
+        },
+        {
+          "type": 12,
+          "items": [
+            {
+              "media": {
+                "url": "https://media.discordapp.net/attachments/1488043526448218233/1488044260874780712/Screenshot_2026-03-29_221112.png?ex=69cb58b6&is=69ca0736&hm=8fc5e7f9fb255acf0b6e195484f7bf8d33021eb9cbf0176eda72aa47c588a448&=&format=webp&quality=lossless"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}).catch(() => {});
+      }
+
+      await interaction.reply({
+  "flags": 32832,
+  "components": [
+    {
+      "type": 17,
+      "components": [
+        {
+          "type": 10,
+          "content": "<a:rose_loading:1488093350933561354> Closing ticket..."
         },
         {
           "type": 14,
@@ -64,13 +97,13 @@ module.exports = {
 
       if (interaction.replied || interaction.deferred) {
         await interaction.followUp({
-          content: "<:rose_xMark:1488048189255716945> An **error** occurred.",
-          ephemeral: true
+          content: "<:rose_xMark:1488048189255716945> An **error** occurred while attempting to close this ticket.",
+          flags: 64
         }).catch(() => {});
       } else {
         await interaction.reply({
-          content: "<:rose_xMark:1488048189255716945> An **error** occurred.",
-          ephemeral: true
+          content: "<:rose_xMark:1488048189255716945> An **error** occurred while attempting to close this ticket.",
+          flags: 64
         }).catch(() => {});
       }
     }
